66a59f295bf3c377c8013fe85564971e39145bda
Krontiva <info@krontiva.africa>
2025-05-19 12:12:38 +0000

    Disable 'react/no-unescaped-entities' rule in ESLint configuration to allow unescaped entities in JSX, enhancing flexibility in component rendering.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,15 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = [
   ...compat.config({
     extends: ['next'],
     rules: {
-      'react/no-unescaped-entities': 'on',
+      'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
