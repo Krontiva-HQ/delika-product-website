8c595ee746ecab1b041b489b26927bd8f4d5c9d0
Krontiva <info@krontiva.africa>
2025-07-09 10:52:41 +0000

    Remove legacy Next.js configuration file and migrate image domain settings to TypeScript configuration

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['api-server.krontiva.africa'],
-  },
-}
-
-module.exports = nextConfig 
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['api-server.krontiva.africa'],
+  },
 };
 
 export default nextConfig;
